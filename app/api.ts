f12dd16870694d6f2bae155b46ac9229b0e114ab
Abdu Mohammed <abdum14@gmail.com>
2025-08-14 11:42:55 +0300

    Update api.ts

--- app/api.ts
+++ app/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const annotationAPI = axios.create({
-  baseURL: "http://172.30.195.249:5800",
+  baseURL: "http://172.16.11.183:5800",
   timeout: 0,
 
 });
 
 export const loaderAPI = axios.create({
-  baseURL: "http://172.30.195.249:8000",
+  baseURL: "http://172.16.11.183:8000",
   timeout: 0,
 
 });
