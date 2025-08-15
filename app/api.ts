fbec36970237b6a1ea059ed47711201a7cc2f0ca
Abdu Mohammed <abdum14@gmail.com>
2025-08-15 09:24:35 +0300

    Update api.ts

--- app/api.ts
+++ app/api.ts
@@ -7,7 +7,7 @@ export const annotationAPI = axios.create({
 });
 
 export const loaderAPI = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://localhost:8001",
   timeout: 0,
 
 });
